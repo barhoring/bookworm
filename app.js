46665e0640ff6d8072f2e7c2ae44f98c1bfc3998
barhoring <bar.apple@gmail.com>
2018-05-14 23:12:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,134 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var session = require('express-session');
+var mongoStore = require('connect-mongo')(session);
+var User = require("./models/user");
+
+function generateOrFindUser(accesToken, refreshToken, profile, done) {
+    if (profile.emails) {
+        User.findOneAndUpdate({
+            email: profile.emails[0].value
+        }, {
+                name: profile.displayName || profile.username,
+                email: profile.emails[0].value,
+                photo: profile.photos[0].value
+            }, {
+                upsert: true
+            }, done);
+    }
+    else {
+        var noEmailError = new Error('Your email privacy settings prevents you from signing into Bookwarm!');
+        done(noEmailError, null);
+    }
+}
+
+// Config GitHub strategy
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/github/return',
+}, generateOrFindUser));
+
+// Config Facebook strategy
+passport.use(
+    new FacebookStrategy({
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: "http://localhost:3000/auth/facebook/return",
+        profileFields: ['id', 'displayName', 'photos', 'email']
+      }, generateOrFindUser)
+);
+
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+});
+// 
+passport.deserializeUser(function (userId, done) {
+    User.findById(userId, done);
+});
+
+var routes = require('./routes/index');
+var auth = require('./routes/auth');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/bookworm-oauth");
+var db = mongoose.connection;
+
+// Session config for Passport and MongoDB
+var sessionOptions = {
+    secret: 'this is supersecret shh!..',
+    resave: true,
+    saveUninitialized: true,
+    store: new mongoStore({
+        mongooseConnection: db,
+    })
+};
+
+app.use(session(sessionOptions));
+
+// Init Passport
+app.use(passport.initialize());
+
+// Restore session
+app.use(passport.session());
+
+// mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+
+app.use('/', routes);
+app.use('/auth', auth);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+
+// GET /auth/login/github
+router.get('/login/github', passport.authenticate('github'));
+
+// GET /auth/github/return
+router.get('/github/return', passport.authenticate('github', {faliureRedirect: '/'}),
+     function (req, res) {
+         // Success auth
+         res.redirect('/profile');
+     }   
+);
+
+// GET /auth/logout
+router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+
+module.exports = router;
